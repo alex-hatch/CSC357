b22a1d42e1ea79a8a6a2dc94d7f3da13b08f6d70
Alex <89677719+alex-hatch@users.noreply.github.com>
2022-11-09 18:27:14 -0800

    Update mytar.h

--- asgn4/mytar.h
+++ asgn4/mytar.h
@@ -5,7 +5,8 @@ uint32_t extract_special_int(const char *where, int len);
 
 int insert_special_int(char *where, size_t size, int32_t val);
 
-int extract_archive(char *tar_file);
+int extract_archive(char *tar_file, char **paths,
+                    int supplied_path, int path_count);
 
 int print_archive();
 
